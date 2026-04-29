924dec562c6d25dbd35fcf26b11730fd934d78ed
Michal Gora <michal.gora@infineon.com>
2026-04-29 10:50:00 +0200

    removing unnecessary variant folders, using prj.conf for CONFIG_BOOTLOADER_MCUBOOT=n

--- variants/arduino_opta_stm32h747xx_m7/variant.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) Arduino s.r.l. and/or its affiliated companies
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#pragma once
--- variants/beagleconnect_freedom/variant.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 Ayush Singh <ayush@beagleboard.org>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-#define PIN_WIRE_SCL D12
-#define PIN_WIRE_SDA D11
